2eb0193f9373ed309e8171717991031a4305b890
Zemi4 <zemi4foto@gmail.com>
2019-11-22 23:51:02 +0300

    add horse

--- The Fastest Horse.py
+++ The Fastest Horse.py
@@ -1,10 +1,14 @@
 ''' если результаты забегов выглядят следующим образом [[“1:13”, “1:26”, “1:11”], [“1:10”, “1:18”, “1:14”], [“1:20”, “1:23”, “1:15”]]
  Тогда самой быстрой лошадью будет №3, так как она победила в 2 забегах из 3.'''
 
+from itertools import groupby
+
 
 def fastest_horse(horses: list) -> int:
-    # replace this for solution
-    return 0
+    winners = [i.index(min(i)) + 1 for i in horses]
+    winning_counts = [(k, len(list(g))) for k, g in groupby(sorted(winners))]
+    winner = sorted(winning_counts, key=lambda x: x[1])[-1][0]
+    return winner
 
 
 if __name__ == '__main__':
@@ -13,4 +17,5 @@ if __name__ == '__main__':
 
     # These "asserts" using only for self-checking and not necessary for auto-testing
     assert fastest_horse([['1:13', '1:26', '1:11'], ['1:10', '1:18', '1:14'], ['1:20', '1:23', '1:15']]) == 3
+    assert fastest_horse([["4:44", "4:11", "4:18"], ["3:10", "3:01", "3:14"], ["2:20", "2:23", "2:15"]]) == 2
     print("Coding complete? Click 'Check' to earn cool rewards!")
